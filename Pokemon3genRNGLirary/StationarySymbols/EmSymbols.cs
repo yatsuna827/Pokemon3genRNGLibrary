f3081aaadc3fa29ac199238360036d305277fb35
yatsuna827 <yatsuna_827@yahoo.co.jp>
2021-03-30 04:55:01 +0900

    シンボルに手を付けかけた

--- Pokemon3genRNGLirary/StationarySymbols/EmSymbols.cs
+++ Pokemon3genRNGLirary/StationarySymbols/EmSymbols.cs
@@ -1,11 +1,42 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace Pokemon3genRNGLibrary.StationarySymbols
 {
     public static class EmSymbols
     {
-        public static StationaryGenerator Latios = new StationaryGenerator(new GBASlot(-1, "ラティオス", 50));
+        public static class EventSymbol
+        {
+            private static readonly GBASlot[] symbols;
+
+            static EventSymbol()
+            {
+                symbols = new GBASlot[]
+                {
+                    new GBASlot(-1, "ラティオス", 50)
+                };
+            }
+        }
+
+        public static class MapSymbol
+        {
+
+        }
+
+        public static class RoamingSymbol
+        {
+            private static readonly GBASlot[] symbols;
+            public static IEnumerable<GBASlot> SelectSymbol(string name = "") => symbols.Where(_ => _.pokemon.Name == name);
+
+            static RoamingSymbol()
+            {
+                symbols = new GBASlot[]
+                {
+                    new GBASlot(-1, "ラティアス", 40),
+                    new GBASlot(-1, "ラティオス", 40),
+                };
+            }
+        }
     }
 }
