e41b381ccd34e9028b129611159a20539baf0bd8
yatsuna827 <yatsuna_827@yahoo.co.jp>
2021-05-16 01:57:10 +0900

    孵化乱数に対応する準備

---
+++ Pokemon3genRNGLirary/EggGenerator/EggGenerator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PokemonStandardLibrary;
+using PokemonStandardLibrary.PokeDex.Gen3;
+using PokemonPRNG.LCG32;
+using PokemonPRNG.LCG32.StandardLCG;
+
+namespace Pokemon3genRNGLibrary
+{
+    class EggGenerator : IGeneratable<Pokemon.Individual>
+    {
+        private readonly Pokemon.Species species;
+        private readonly uint fixedPID;
+        private readonly uint[][] parentsIVs;
+        private readonly IIVsGenerator ivsGenerator;
+
+
+        private readonly uint[] permutarion1 = new uint[6] { 0, 1, 2, 5, 3, 4 };
+        private readonly uint[] permutarion2 = new uint[5] {    1, 2, 5, 3, 4 };
+        private readonly uint[] permutarion3 = new uint[4] {    1,    5, 3, 4 };
+
+        public Pokemon.Individual Generate(uint seed)
+        {
+            var ivs = ivsGenerator.GenerateIVs(ref seed);
+
+            seed.Advance();
+
+            var idx1 = permutarion1[seed.GetRand(6)];
+            var idx2 = permutarion2[seed.GetRand(5)];
+            var idx3 = permutarion3[seed.GetRand(4)];
+
+            ivs[idx1] = parentsIVs[seed.GetRand() & 1][idx1];
+            ivs[idx2] = parentsIVs[seed.GetRand() & 1][idx2];
+            ivs[idx3] = parentsIVs[seed.GetRand() & 1][idx3];
+
+            return species.GetIndividual(5, ivs, fixedPID);
+        }
+
+        private EggGenerator(Pokemon.Species spe, uint pid, uint[] parent1, uint[] parent2, IIVsGenerator gen)
+            => (species, fixedPID, parentsIVs, ivsGenerator) = (spe, pid, new[] { parent1, parent2 }, gen);
+
+        public static EggGenerator CreateInstance(Pokemon.Species species, uint pid, uint[] parent1, uint[] parent2)
+        {
+            return null;
+        }
+    }
+}
